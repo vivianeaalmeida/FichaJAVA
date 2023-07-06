86693699105eb9d80ff5da811cb604aa9b1f83e5
Viviane Andrade de Almeida <viviane.aalmeida@live.com>
2023-07-06 09:54:53 +0100

    updates

--- src/PL05/Exercise_07.java
+++ src/PL05/Exercise_07.java
@@ -8,6 +8,7 @@ public class Exercise_07 {
         System.out.println("*** PL 05 EX 07 ***");
         int[] vetor = new int[10];
         int maiorValor;
+        boolean haPares = false;
         System.out.println("Insira um numero: ");
         vetor[0] = input.nextInt();
         maiorValor = vetor[0];
@@ -15,13 +16,37 @@ public class Exercise_07 {
         for(int i = 1; i < 10; i++){
             System.out.println("Insira um valor: ");
             vetor[i] = input.nextInt();
-            if(vetor[i] > maiorValor){
+            if(vetor[i] > maiorValor && vetor[i] % 2 == 0){
                 maiorValor = vetor[i];
             }
         }
-        System.out.println("Maior valor par: " + maiorValor);
+
+        for(int i = 0; i < vetor.length; i++){
+            if(vetor[i] % 2 == 0){
+                haPares = true;
+            }
+        }
+
+        if(haPares == true){
+            System.out.println("Maior valor par: " + maiorValor);
+        } else {
+            System.out.println("Não há maior valor par");
+        }
 
     }
 }
 
 //falta o valor par.
+
+/*
+ else if (maiorValor % 2 == 1) {
+                impar = true;
+            }
+
+ if(impar == true){
+            System.out.println("Não há valores pares");
+        } else {
+            System.out.println("Maior valor par: " + maiorValor);
+        }
+
+ */
