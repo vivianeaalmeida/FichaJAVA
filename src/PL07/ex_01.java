c9b369c1e4692b73702de7bebeb1a10ab73c9fb5
Viviane Andrade de Almeida <viviane.aalmeida@live.com>
2023-07-20 12:26:01 +0100

    updates

--- src/PL07/ex_01.java
+++ src/PL07/ex_01.java
@@ -1,10 +1,24 @@
 package PL07;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class ex_01 {
-    public static void main(String[] args) {
+    public static void lerLinhasInteiras() throws FileNotFoundException{
+        Scanner in = new Scanner(new File("ficheiros/exercicio_01.txt"));
+        String linha;
 
-        File file = new File("ficheiros/exercicio.txt");
+        while (in.hasNextLine()){
+            linha = in.nextLine();
+            System.out.println(linha);
+        }
+        in.close();
+    }
+    public static void main(String[] args) {
+        lerLinhasInteiras();
     }
 }
+
+
+//File file = new File("ficheiros/exercicio.txt");
