f5246cdf80459031fc05b736b8a093094d944ffd
Viviane Andrade de Almeida <viviane.aalmeida@live.com>
2023-07-24 16:53:37 +0100

    litt

--- src/PL07/ex_04.java
+++ src/PL07/ex_04.java
@@ -1,12 +1,6 @@
 package PL07;
 
-import java.util.Scanner;
-
 public class ex_04 {
-
-    public static void lerLinhasFicheiro(){
-        Scanner in = new Scanner()
-    }
     public static void main(String[] args) {
 
     }
